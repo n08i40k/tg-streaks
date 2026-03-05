3a1247ce440846f7768069645b435ed4ef7b63a8
n08i40k <nhomcencko1@gmail.com>
2026-03-06 01:47:31 +0400

    chore: remove redunant variable definition

--- plugin/tg-streaks.py
+++ plugin/tg-streaks.py
@@ -87,7 +87,6 @@ DAY_CHECK_REQUEST_DELAY_SECONDS = 0.35
 DAY_CHECK_RETRY_DELAY_SECONDS = 0.8
 DAY_CHECK_MESSAGE_TABLE_CANDIDATES = ("messages_v2", "messages")
 DEX_URL = f"https://github.com/{__author__.replace('@', '')}/{__id__}/releases/download/{__version__}/classes.dex"
-DEX_SHA256 = ""
 DEX_SHA256 = "3758ca9a813ba0a0c22c56303acdd5951791cefedd1ef8aa826f0c8889017d68"
 SERVER_TIMEZONE_ID = "UTC"
 DEBUG_MODE = False
