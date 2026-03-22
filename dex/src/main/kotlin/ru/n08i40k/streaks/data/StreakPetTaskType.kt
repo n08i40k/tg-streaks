2d8cea00b1f359647932fd69a4d6f47f68c8a540
n08i40k <nhomcencko1@gmail.com>
2026-03-22 23:55:32 +0400

    feat: add points for every task

--- dex/src/main/kotlin/ru/n08i40k/streaks/data/StreakPetTaskType.kt
+++ dex/src/main/kotlin/ru/n08i40k/streaks/data/StreakPetTaskType.kt
@@ -1,9 +1,9 @@
 package ru.n08i40k.streaks.data
 
-enum class StreakPetTaskType {
-    EXCHANGE_ONE_MESSAGE,
-    SEND_FOUR_MESSAGES_EACH,
-    SEND_TEN_MESSAGES_EACH,
+enum class StreakPetTaskType(val points: Int) {
+    EXCHANGE_ONE_MESSAGE(1),
+    SEND_FOUR_MESSAGES_EACH(2),
+    SEND_TEN_MESSAGES_EACH(4),
     ;
 
     val defaultPayload: StreakPetTaskPayload
