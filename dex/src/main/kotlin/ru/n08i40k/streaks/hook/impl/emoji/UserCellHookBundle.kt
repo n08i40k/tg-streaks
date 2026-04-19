cded7fc284c40bf349bb6b3a1a91d4eec274718b
n08i40k <nhomcencko1@gmail.com>
2026-04-20 02:55:56 +0400

    fix: UserCell view

--- dex/src/main/kotlin/ru/n08i40k/streaks/hook/impl/emoji/UserCellHookBundle.kt
+++ dex/src/main/kotlin/ru/n08i40k/streaks/hook/impl/emoji/UserCellHookBundle.kt
@@ -6,17 +6,16 @@
 
 package ru.n08i40k.streaks.hook.impl.emoji
 
-import android.graphics.drawable.Drawable
+import org.telegram.tgnet.TLRPC
 import org.telegram.ui.ActionBar.SimpleTextView
 import org.telegram.ui.Cells.UserCell
-import org.telegram.ui.Components.AnimatedEmojiDrawable
 import ru.n08i40k.streaks.hook.HookBundle
 import ru.n08i40k.streaks.hook.InstallHook
 import ru.n08i40k.streaks.override.StreakEmoji
 import ru.n08i40k.streaks.util.getField
 import ru.n08i40k.streaks.util.getFieldValue
 
-class UserCellHookBundle : HookBundle(){
+class UserCellHookBundle : HookBundle() {
     override fun inject(
         before: InstallHook,
         after: InstallHook
@@ -31,60 +30,22 @@ class UserCellHookBundle : HookBundle(){
             val thisObject = param.thisObject as UserCell
             val thisClass = UserCell::class.java
 
-            val dialogId = getFieldValue<Long>(thisClass, thisObject, "dialogId")!!
-
-            if (dialogId < 0)
-                return@after
+            val currentUser =
+                getFieldValue<Object>(thisClass, thisObject, "currentObject") as? TLRPC.User
+                    ?: return@after
 
             val nameTextView =
                 getFieldValue<SimpleTextView>(thisClass, thisObject, "nameTextView")!!
 
-            StreakEmoji.encapsulate(
+            val emoji = StreakEmoji.encapsulate(
                 thisObject,
                 getField(thisClass, "emojiStatus"),
                 null,
-                dialogId,
+                currentUser.id,
                 nameTextView = nameTextView
             )
 
-            val rightDrawable = getFieldValue<Drawable>(nameTextView, "rightDrawable")
-            val rightDrawable2 = getFieldValue<Drawable>(nameTextView, "rightDrawable2")
-
-            val emojiStatus =
-                getFieldValue<AnimatedEmojiDrawable.SwapAnimatedEmojiDrawable>(
-                    thisClass,
-                    thisObject,
-                    "emojiStatus"
-                )
-            val emojiStatus2 =
-                getFieldValue<AnimatedEmojiDrawable.SwapAnimatedEmojiDrawable>(
-                    thisClass,
-                    thisObject,
-                    "emojiStatus2"
-                )
-
-            val newEmojiStatus =
-                getFieldValue<AnimatedEmojiDrawable.SwapAnimatedEmojiDrawable>(
-                    thisClass,
-                    thisObject,
-                    "emojiStatus"
-                )
-            val newEmojiStatus2 =
-                getFieldValue<AnimatedEmojiDrawable.SwapAnimatedEmojiDrawable>(
-                    thisClass,
-                    thisObject,
-                    "emojiStatus2"
-                )
-
-            if (rightDrawable === emojiStatus)
-                nameTextView.rightDrawable = newEmojiStatus
-            else if (rightDrawable === emojiStatus2)
-                nameTextView.rightDrawable = newEmojiStatus2
-
-            if (rightDrawable2 === emojiStatus)
-                nameTextView.rightDrawable2 = newEmojiStatus
-            else if (rightDrawable2 === emojiStatus2)
-                nameTextView.rightDrawable2 = newEmojiStatus2
+            nameTextView.rightDrawable = emoji
         }
     }
 }
