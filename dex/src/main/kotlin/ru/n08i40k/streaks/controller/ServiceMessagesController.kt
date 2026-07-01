60b10b26eabc99220f1de5d65cfa4d6dba1b54fe
n08i40k <nhomcencko1@gmail.com>
2026-07-01 20:26:26 +0400

    fix: crash due to changes in prepareSendingText method on 12.7.0 and above

--- dex/src/main/kotlin/ru/n08i40k/streaks/controller/ServiceMessagesController.kt
+++ dex/src/main/kotlin/ru/n08i40k/streaks/controller/ServiceMessagesController.kt
@@ -87,39 +87,83 @@ class ServiceMessagesController {
         send(accountId, peerUserId, ServiceMessage.RESTORE_TEXT)
     }
 
-    private val isLegacyVersion by lazy { isClientVersionBelow("12.2.0") }
+    @Suppress("EnumEntryName")
+    private enum class SendMessagesHelperRev {
+        Pre_12_2_0,
+        Pre_12_7_0,
+        Latest;
+    }
+
+    private val currentRevision by lazy {
+        if (isClientVersionBelow("12.2.0"))
+            return@lazy SendMessagesHelperRev.Pre_12_2_0
+
+        if (isClientVersionBelow("12.7.0"))
+            return@lazy SendMessagesHelperRev.Pre_12_7_0
+
+        return@lazy SendMessagesHelperRev.Latest
+    }
 
     private val prepareSendingText by lazy {
-        if (isLegacyVersion) {
-            SendMessagesHelper::class.java.getDeclaredMethod(
-                "prepareSendingText",
-                AccountInstance::class.java,
-                String::class.java,
-                Long::class.java,
-                Boolean::class.java,
-                Int::class.java,
-                Long::class.java
-            )
-        } else {
-            SendMessagesHelper::class.java.getDeclaredMethod(
-                "prepareSendingText",
-                AccountInstance::class.java,
-                String::class.java,
-                Long::class.java,
-                Boolean::class.java,
-                Int::class.java,
-                Int::class.java,
-                Long::class.java
-            )
+        when (currentRevision) {
+            SendMessagesHelperRev.Pre_12_2_0 ->
+                SendMessagesHelper::class.java.getDeclaredMethod(
+                    "prepareSendingText",
+                    AccountInstance::class.java,
+                    String::class.java,
+                    Long::class.java,
+                    Boolean::class.java,
+                    Int::class.java,
+                    Long::class.java
+                )
+
+            SendMessagesHelperRev.Pre_12_7_0 ->
+                SendMessagesHelper::class.java.getDeclaredMethod(
+                    "prepareSendingText",
+                    AccountInstance::class.java,
+                    String::class.java,
+                    Long::class.java,
+                    Boolean::class.java,
+                    Int::class.java,
+                    Int::class.java,
+                    Long::class.java
+                )
+
+            SendMessagesHelperRev.Latest ->
+                SendMessagesHelper::class.java.getDeclaredMethod(
+                    "prepareSendingText",
+                    AccountInstance::class.java,
+                    CharSequence::class.java,
+                    Long::class.java,
+                    Boolean::class.java,
+                    Int::class.java,
+                    Int::class.java,
+                    Long::class.java
+                )
         }
     }
 
     private fun send(accountId: Int, peerUserId: Long, message: String) {
         val account = AccountInstance.getInstance(accountId)
 
-        if (isLegacyVersion)
-            prepareSendingText.invoke(null, account, message, peerUserId, false, 0, 0L)
-        else
-            prepareSendingText.invoke(null, account, message, peerUserId, false, 0, 0, 0L)
+        when (currentRevision) {
+            SendMessagesHelperRev.Pre_12_2_0 ->
+                prepareSendingText.invoke(null, account, message, peerUserId, false, 0, 0L)
+
+            SendMessagesHelperRev.Pre_12_7_0 ->
+                prepareSendingText.invoke(null, account, message, peerUserId, false, 0, 0, 0L)
+
+            SendMessagesHelperRev.Latest ->
+                prepareSendingText.invoke(
+                    null,
+                    account,
+                    message as CharSequence,
+                    peerUserId,
+                    false,
+                    0,
+                    0,
+                    0L
+                )
+        }
     }
 }
