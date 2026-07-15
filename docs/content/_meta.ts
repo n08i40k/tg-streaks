2cc9cc277ff3efb99a7c72673f47ea187e4e8521
n08i40k <nhomcencko1@gmail.com>
2026-07-16 01:44:45 +0400

    feat(docs): update information according to the latest release
    Signed-off-by: n08i40k <nhomcencko1@gmail.com>

--- docs/content/_meta.ts
+++ docs/content/_meta.ts
@@ -3,6 +3,7 @@ export default {
   installation: "Установка и обновление",
   features: "Возможности",
   "chat-menu": "Меню чата",
+  "control-panel": "Панель управления",
   settings: "Настройки",
   backups: "Бэкапы и перенос данных",
   updates: "Обновления плагина",
